35b1609067ac3dade514fe1d85c7d54d4320ce48
Carlos Cardenas <unimauro@gmail.com>
2020-06-03 20:28:09 -0500

    Updating All

---
+++ read_dir.py
@@ -0,0 +1,10 @@
+import os 
+l=os.listdir("/home/unimauro/Develop/TDD-KATA") 
+
+for i in l:
+    if i.find(".")<0:
+            print(i)
+            l=open(i+"/Readme.md","w")
+            l.writelines(i.upper())
+            l.close()
+
